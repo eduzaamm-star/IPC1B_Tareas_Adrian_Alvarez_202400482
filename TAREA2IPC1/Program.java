b7a20bcc034c6cd2db920d9c096953c08d210524
Meduzki zam <eduzaamm@gmail.com>
2026-08-08 19:23:33 -0600

    estructura y variables terminadas

--- TAREA2IPC1/Program.java
+++ TAREA2IPC1/Program.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        // Declaración de variables
+        int cantidadJugadores = 5;
+        double pingPromedio = 0.0;
+        boolean servidorActivo = true;
+        char rangoDefault = 'U';
+
+        // menu inicial
+        System.out.println("====SISTEMA DE ESTADISTICAS====:");
+        System.out.println("Servidor activo: " + servidorActivo);
     }
 }
+
