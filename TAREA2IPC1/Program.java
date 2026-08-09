7303c2bad08704955a962dfbf3368846598eccc3
Meduzki zam <eduzaamm@gmail.com>
2026-08-08 19:49:40 -0600

    Metodo de validacion con Scanner

--- TAREA2IPC1/Program.java
+++ TAREA2IPC1/Program.java
@@ -1,17 +1,48 @@
 import java.util.Scanner;
-
 public class Program {
+
+    // Método 1: main 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Declaración de variables
+        
+        // M1: Declaración de variables: 4 tipos primitivos 
         int cantidadJugadores = 5;
-        double pingPromedio = 0.0;
+        double pingPromedio = 0.0; 
         boolean servidorActivo = true;
-        char rangoDefault = 'U';
+        char rangoDefault = 'U'; 
 
-        // menu inicial
-        System.out.println("====SISTEMA DE ESTADISTICAS====:");
-        System.out.println("Servidor activo: " + servidorActivo);
+        // ENCABEZADO
+        System.out.println("==== SISTEMA DE ESTADISTICAS MCGUAT ====");
+        System.out.println("Estado del Servidor: " + (servidorActivo ? "ONLINE" : "OFFLINE"));
+        System.out.println("Rango de nuevos jugadores: [" + rangoDefault + "]");
+        
+        // Llamada a los métodos modulares
+        procesarPingsUnidimensional(scanner, cantidadJugadores);
+        procesarRecursosBidimensional(scanner);
+        
+        System.out.println("\n==== SISTEMA CERRADO ====");
+        scanner.close();
     }
-}
 
+    // M2: Validación de entrada: Rechazo de letras y números fuera de rango
+    public static int solicitarEnteroValido(Scanner scanner, String mensaje, int min, int max) {
+        int numero = 0;
+        boolean datoValido = false;
+
+        do {
+            System.out.print(mensaje);
+            if (scanner.hasNextInt()) { // verificacion de entero
+                numero = scanner.nextInt();
+                if (numero >= min && numero <= max) {
+                    datoValido = true; 
+                } else {
+                    System.out.println("Error: El valor debe estar entre " + min + " y " + max + "."); // mensaje de error
+                }
+            } else {
+                System.out.println("Error: Ingreso inválido. Debes ingresar un número entero."); // mensaje de error
+                scanner.next(); // 
+            }
+        } while (!datoValido); // repetir hasta que el dato sea valido
+
+        return numero;
+    }
